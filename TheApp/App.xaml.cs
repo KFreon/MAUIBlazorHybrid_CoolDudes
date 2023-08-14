19bad4db02ab1f777813704b9933b198baeca776
Kael Larkin <kael.larkin@outlook.com>
2023-08-14 14:19:01 +1000

    Add project files.

---
+++ TheApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TheApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
